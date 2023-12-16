ad101eb6ba27a5c49596cb55400aa574f8faa94f
RadwanHegazy <radwangaber22@gmail.com>
2023-12-16 18:09:42 +0200

    add quran and reel in installed_apps

--- backend/settings.py
+++ backend/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
     'rest_framework'
     
+    'users',
+    'quran',
+    'reel',
 
 ]
 
