e860c3608cfdce206c3e2e161e8f3817d3517604
RadwanHegazy <radwangaber22@gmail.com>
2023-12-24 13:17:56 +0200

    add registeration for users

---
+++ users/apis/register.py
@@ -0,0 +1,32 @@
+from users.models import User
+from rest_framework import status, decorators
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+
+@decorators.api_view(['POST'])
+def RegisterView (request) : 
+    try : 
+
+        user = User.objects.create_user(
+            email = request.data.get('email',None),
+            password = request.data.get('password',None),
+            full_name = request.data.get('full_name',None),
+            picture = request.data.get('picture',None),
+        )
+
+
+        tokens = RefreshToken.for_user(user=user)
+
+        tokens = {
+            'access' : str(tokens.access_token),
+            'refresh' : str(tokens)
+        }
+
+        return Response(tokens,status=status.HTTP_200_OK)
+    
+    except Exception as error :
+        return Response({
+            'message' : f'an error accured : {error}'
+        },status=status.HTTP_400_BAD_REQUEST)
--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
+from .apis.register import RegisterView
 
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/',TokenRefreshView.as_view()),
+    path('register/',RegisterView),
 ]
